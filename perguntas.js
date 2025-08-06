c6971556d0fd6fe7bf855e9121dbac486afca504
lorraine0212 <goncalves.lorraine@escola.pr.gov.br>
2025-08-06 16:12:21 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,1 +1,24 @@
+criaCartao(
+    'Geografia',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Geografia',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Geografia',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
 
